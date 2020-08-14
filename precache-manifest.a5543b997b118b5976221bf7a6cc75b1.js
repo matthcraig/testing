ce79befde399b707d9aeb60ae4063f6861aef812
Matthew Craig <mgcraig78@gmail.com>
2020-08-14 15:03:59 -0700

    Updates

---
+++ precache-manifest.a5543b997b118b5976221bf7a6cc75b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57dbdb9a3e69144dc3b1eb4129921a6e",
+    "url": "/testing/index.html"
+  },
+  {
+    "revision": "0c27c80df65192df5e7d",
+    "url": "/testing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3bd36651dc54d2d5933d",
+    "url": "/testing/static/js/2.7107709c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testing/static/js/2.7107709c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c27c80df65192df5e7d",
+    "url": "/testing/static/js/main.2e0cdce7.chunk.js"
+  },
+  {
+    "revision": "7c73a9d32c9bbe4e7790",
+    "url": "/testing/static/js/runtime-main.f0c11d5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testing/static/media/logo.5d5d9eef.svg"
+  }
+]);
