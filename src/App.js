1af1bd7ee997d29e1127137d315242ba52fa95cf
Matthew Craig <mgcraig78@gmail.com>
2020-08-14 14:55:19 -0700

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>BURGER KING</h2>
+        </div>
+        <p className="App-intro">
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Square.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/*something*/}
+      </button>
+    );
+  }
+}
+export default Square;
